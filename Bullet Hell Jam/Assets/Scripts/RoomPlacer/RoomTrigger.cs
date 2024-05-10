80cdd257cbffa39870077fce457a06540ac32a98
Posunkin <vposunkin@gmail.com>
2024-05-11 00:53:56 +0300

    Mini map

--- Bullet Hell Jam/Assets/Scripts/RoomPlacer/RoomTrigger.cs
+++ Bullet Hell Jam/Assets/Scripts/RoomPlacer/RoomTrigger.cs
@@ -2,18 +2,32 @@ using UnityEngine;
 
 public class RoomTrigger : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer _minimapIcon;
+    private Color _defaultColor;
+    private Color _enterColor;
     private Room _room;
 
     private void Start()
     {
         _room = GetComponent<Room>();
+        _defaultColor = _minimapIcon.color;
+        _enterColor = Color.white;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<PlayerStats>() != null)
         {
+            _minimapIcon.color = _enterColor;
             _room.Enter();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerStats>() != null)
+        {
+            _minimapIcon.color = _defaultColor;
         }
     }
 }
